f9ef74a56bb1eb334cb9947379c9c2bd65dbe06d
SanjayDutta <sanjaydutta976@gmail.com>
2025-01-07 18:07:47 +0530

    user, stats routes and controller

--- server/src/routes/stats.route.js
+++ server/src/routes/stats.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getStats } from "../controller/stats.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("this is stats route");
-});
+router.get("/", protectRoute, requireAdmin, getStats);
 
 export default router;
