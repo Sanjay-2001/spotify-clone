636a041ba831baf7aec39dcd7af279285020f4e9
SanjayDutta <sanjaydutta976@gmail.com>
2025-01-08 15:57:38 +0530

    album page

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,6 @@
 import HomePage from "./home/HomePage";
 import AuthCallbackPage from "./auth/AuthCallbackPage";
 import ChatPage from "./chat/ChatPage";
+import AlbumPage from "./album/AlbumPage";
 
-export { HomePage, AuthCallbackPage, ChatPage };
+export { HomePage, AuthCallbackPage, ChatPage, AlbumPage };
