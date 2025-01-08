81eb0fcc94d04d2ea126db49d91f31ab5d2c8c65
SanjayDutta <sanjaydutta976@gmail.com>
2025-01-08 11:51:13 +0530

    main layout and left sidebar

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,5 @@
 import HomePage from "./home/HomePage";
-import AuthCallbackPage from "./auth-callback/AuthCallbackPage";
+import AuthCallbackPage from "./auth/AuthCallbackPage";
+import ChatPage from "./chat/ChatPage";
 
-export { HomePage, AuthCallbackPage };
+export { HomePage, AuthCallbackPage, ChatPage };
